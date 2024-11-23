65650194c6eb2ea507b6ac9b276cf2ac1c97661c
Tempest02002 <raghuvanshidhruv@gmail.com>
2024-11-24 00:10:48 +0530

    Done

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 
 // Create a connection
+// we can also use .env but since it is in git ignore I have to remove it  and directly add details
 const conn = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
